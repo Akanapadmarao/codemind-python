6a40f94d1c75d0fdbcfbfeaec549c45b89063850
Akanapadmarao <96292930+Akanapadmarao@users.noreply.github.com>
2022-10-31 14:36:23 +0530

    a=int(input()) b=int(input()) # proper factor sum of a pfs_a=0 for i in range(1,a):     if a%i==0:         pfs_a+=i # proper factor sum of b pfs_b=0 for i in range(1,b):     if b%i==0:         pfs_b+=i
    if pfs_a== b and pfs_b== a:
        print("Amicable")
    else:
        print('Not Amicable')

--- Amicable_Numbers.py
+++ Amicable_Numbers.py
@@ -1,14 +1,17 @@
-x=int(input())
-y=int(input())
-s1=0
-s2=0
-for i in range(1,x):
-    if x%i==0:
-        s1=s1+i
-for j in range(1,y):
-    if y%j==0:
-        s2=s2+j
-if (s1==y and s2==x):
-    print('Amicable')
+a=int(input())
+b=int(input())
+# proper factor sum of a
+pfs_a=0
+for i in range(1,a):
+    if a%i==0:
+        pfs_a+=i
+# proper factor sum of b
+pfs_b=0
+for i in range(1,b):
+    if b%i==0:
+        pfs_b+=i
+
+if pfs_a== b and pfs_b== a:
+    print("Amicable")
 else:
     print('Not Amicable')
